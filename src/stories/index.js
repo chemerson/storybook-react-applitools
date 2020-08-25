235312522f3b4c22e967abc916978c5c7ab5cf34
Chris Emerson <cme210@yahoo.com>
2020-08-25 17:39:42 -0400

    Update index.js

--- src/stories/index.js
+++ src/stories/index.js
@@ -19,11 +19,20 @@ storiesOf('Button', module)
     </Button>
   ));
 
+  storiesOf('Button 2', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button!</Button>)
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
+
   storiesOf('App', module).add('default', () => <App />);
 
   // added cme
-
-  storiesOf("Pages/Welcome", module)
+  storiesOf("Button", module)
   .addDecorator(withKnobs)
   .add("Welcome", () => (
    <Welcome>
@@ -36,12 +45,15 @@ storiesOf('Button', module)
    </Welcome>
   )); 
 
-  storiesOf('BarChart', module)
-  .addDecorator(getStory => <ShowcaseCard>{ getStory() }</ShowcaseCard>)
-  .add('With Multiple data set', () => <MultipleBarChart/>)
-  .add('With Gradient', () => <BarChartWithGradient/>)
-  .add('With Different Bars', () => <BarChartWithDifferentBars/>)
-  .add('Horizontal', () => <BarChartHorizontal/>)
-  .add('Horizontal with YAxis', () => <BarChartHorizontalWithYAxis/>)
-  .add('Horizontal with Labels', () => <BarChartHorizontalWithLabels/>)
-  .add('Vertical with Labels', () => <BarChartVerticalWithLabels/>)
+  storiesOf("Button 2", module)
+  .addDecorator(withKnobs)
+  .add("Welcome", () => (
+   <Welcome>
+    <Table
+     items={[
+      { id: 1, name: "foo", description: "bar" },
+      { id: 2, name: "baz", description: "foobar" }
+     ]}
+    />
+   </Welcome>
+  )); 
