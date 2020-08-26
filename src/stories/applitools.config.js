e9421c631762b2db1c89b7ead7a043f154e8e593
Chris Emerson <cme210@yahoo.com>
2020-08-26 14:27:04 -0400

    Update applitools.config.js

--- src/stories/applitools.config.js
+++ src/stories/applitools.config.js
@@ -3,6 +3,7 @@ module.exports = {
   batchName: "Storybook 1",
   browser: [
     { width: 800, height: 600, name: "firefox" },
+    { width: 1024, height: 768, name: "firefox" },
     { width: 1024, height: 768, name: "chrome" }
   ]
 };
