9f7e6758d5ce7ebaf84faaaaf82fc42d8d4b1a97
Chris Emerson <cme210@yahoo.com>
2020-08-27 09:53:34 -0400

    Update applitools.config.js

--- applitools.config.js
+++ applitools.config.js
@@ -4,7 +4,6 @@ module.exports = {
   browser: [
     { width: 800, height: 600, name: "firefox" },
     { width: 1024, height: 768, name: "firefox" },
-    { width: 1024, height: 768, name: "chrome" },
-    { width: 1024, height: 768, name: "safari" }
+    { width: 1024, height: 768, name: "chrome" }
   ]
 };
